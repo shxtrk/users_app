05c78e17092553a4cf76c83f5f84f6f749023942
shxtrk <direct@shxtrk.dev>
2023-03-06 09:45:51 +0200

    Project init

---
+++ Users/UsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersApp.swift
+//  Users
+//
+//  Created by Serhii Striuk on 06.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct UsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
